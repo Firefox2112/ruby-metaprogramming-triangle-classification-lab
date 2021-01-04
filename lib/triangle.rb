81704a94d5a4f495fb5393eee2380057ec38d403
Coby <cobyrpilpel2@gmail.com>
2021-01-04 03:22:49 -0500

    commit tree 74ae070d2232302eb4b0a912ccb4c0ffe15c0e3c

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
-  # write code here
+attr_accessor :sideA, :sideB, :sideC
+
+def initialize(sideA, sideB, sideC)
+  @sideA = sideA
+  @sideB = sideB
+  @sideC = sideC
+end
+
+def valid?
+array = [@sideA, @sideB, @sideC].sort
+if [@sideA, @sideB, @sideC].any? {|each| each <= 0} || array.pop >= array[0] += array[1]
+    raise TriangleError
+end
+end
+
+
+
+
+
+def kind
+  if valid?
+  else
+  if [@sideA,@sideB,@sideC].uniq.length == 1
+    :equilateral
+  elsif
+    [@sideA,@sideB,@sideC].uniq.length == 2
+    :isosceles
+  elsif
+    [@sideA,@sideB,@sideC].uniq.length == 3
+    :scalene
+  end
+  end
+end
+
+
+class TriangleError < StandardError
+  def message
+    "This is not a legal triangle"
+  end
+end
+
+
 end
